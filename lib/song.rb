51b204a0985b6e39eadd35aaa071a7c1dfc9abd4
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-28 17:19:51 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,70 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find {|song| name == song.name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    song ? song : self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    
+    #ARTIST - TITLE.EXTENSION
+    file_parts = filename.match(/(^.+)(?:\s[-]\s)(.+)(?:[.])(.+)/)
+    
+    artist = file_parts[1]
+    name = file_parts[2]
+    extension = file_parts[3]
+    
+    song = self.new_by_name(name)
+    song.artist_name = artist
+    
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    
+    #ARTIST - TITLE.EXTENSION
+    file_parts = filename.match(/(^.+)(?:\s[-]\s)(.+)(?:[.])(.+)/)
+    
+    artist = file_parts[1]
+    name = file_parts[2]
+    extension = file_parts[3]
+    
+    song = self.create_by_name(name)
+    song.artist_name = artist
+    
+    song
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
 end
